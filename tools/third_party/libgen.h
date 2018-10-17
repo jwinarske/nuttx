53ed60147222460c088fc31eb96f9648d7d3cc5a
Joel Winarske <joel.winarske@gmail.com>
2018-10-17 16:57:23 -0700

    basename implementation

--- tools/third_party/libgen.h
+++ tools/third_party/libgen.h
@@ -7,10 +7,39 @@
 char  *basename(char *);
 char  *dirname(char *);
 
-static char* basename(char *path)
+static char *basename(char *str)
 {
-    return path;
+    char *p;
+	size_t len = strlen(str);
+    
+	/* skip leading slash.  Case is /usr/, /usr/tst/ is fine */
+	if (len > 1) {
+		if ((*str == '\\') || (*str == '/')) {
+			str += 1;
+			len--;
+		}
+	}
+
+	p = str + len - 1;
+	if (len > 1) {
+		if ((*p == '\\') || (*p == '/')) {
+			if (*(p - 1) == ':')
+				return str;
+			*p = '\0';
+			p--;
+		}
+	}
+
+	for (; str < p; p--) {
+		if ((*p == '\\') || (*p == '/')) {
+			p++;
+			break;
+		}
+	}
+
+	return p;
 }
+
 static char* dirname(char *path)
 {
     return path;
